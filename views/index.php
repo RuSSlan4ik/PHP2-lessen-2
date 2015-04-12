f7de83a3de6b19d37e30cb21b0763e04d64b0f72
RuSSlan4ik <ru.hus@mail.ru>
2015-04-13 00:14:44 +0300

    создание

---
+++ views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head lang="en">
+  <meta charset="UTF-8" />
+  <title></title>
+</head>
+<body>
+<h1>Новости</h1>
+<div><a href="../lesson1/form.php"><button>Добавить новость</button></a></div>
+<?php foreach ($news as $article) : ?>
+  <article>
+    <h3>
+      <a href="../lesson1/article.php?id=<?php echo $article['id']; ?>">
+        <?php echo $article['title']; ?>
+      </a>
+    </h3>
+    <div><?php echo $article['text_news']; ?></div>
+  </article>
+<?php endforeach; ?>
+</body>
+</html>
