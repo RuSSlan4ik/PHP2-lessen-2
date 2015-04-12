faa3db551cb9b035f733b9fdd074116e7a3b5e6b
RuSSlan4ik <ru.hus@mail.ru>
2015-04-13 01:51:00 +0300

    убрано post

--- form.php
+++ form.php
@@ -1,6 +1,3 @@
 <?php
-include __DIR__ . '/views/form.php';
-require __DIR__ . '/models/one.php';
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  addNews($_POST['author_name'], $_POST['title'], $_POST['text_news'], $_POST['data']);
-}
+require __DIR__ . '/views/form.php';
+include __DIR__ . '/models/one.php';
