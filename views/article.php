568deba8af37ac467c146477a7ad62e20ca4f170
RuSSlan4ik <ru.hus@mail.ru>
2015-04-13 00:16:18 +0300

    создание

---
+++ views/article.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+<h1>Новость</h1>
+<article>
+  <h3>
+    <?php echo $article['title']; ?>
+  </h3><p><?php echo $article['data']; ?>  Автор новости:<?php echo $article['author_name']; ?></p>
+
+  <div><?php echo $article['text_news']; ?></div>
+</article>
+<br/>
+<div><a href="../lesson1/">Назад</a></div>
+</body>
+</html>
