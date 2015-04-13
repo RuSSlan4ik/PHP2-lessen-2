392b9adc2474a009d66b92c562dab0f09f8799bb
RuSSlan4ik <ru.hus@mail.ru>
2015-04-13 18:24:55 +0300

    Parse error

--- models/news.php
+++ models/news.php
@@ -3,9 +3,10 @@ require_once __DIR__ . '/../classes/Data.inc';
 require_once __DIR__ . '/../classes/Article.inc';
 require_once __DIR__ . '/../classes/News.inc';
 
-$anews = new News($title, $text, $author, $date);
+$anews = new News();
 $tablename = $anews->setTableName();
-$sql = $anews->addRecord($tablename);
-$namedb = 'Lesson';
-$db = new DataB(DataB::DATABASE_HOST, DataB::DATABASE_USER, DataB::DATABASE_PSW, $namedb);
-$news = $db->changeData($sql);
+$sql = $anews->getAllData($tablename);
+
+$db = new DataB('title', 'text', 'author','date');
+$log->connect('localhost', 'root', '', 'Lesson');
+$news = $db->getData($sql);
