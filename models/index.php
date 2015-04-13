39c545fee1d0e6e7f2d25bea4829b4ec2b4c1417
RuSSlan4ik <ru.hus@mail.ru>
2015-04-13 18:25:07 +0300

    Parse error

--- models/index.php
+++ models/index.php
@@ -3,9 +3,14 @@ require_once __DIR__ . '/../classes/Data.inc';
 require_once __DIR__ . '/../classes/Article.inc';
 require_once __DIR__ . '/../classes/News.inc';
 
+
 $anews = new News();
 $tablename = $anews->setTableName();
-$sql = $anews->getAllData($tablename);
-$namedb = 'Lesson';
-$db = new DataB(DataB::DATABASE_HOST, DataB::DATABASE_USER, DataB::DATABASE_PSW, $namedb);
-$news = $db->getData($sql);
+$sql = $anews->getAllData('news');
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpassword = 'no';
+$dbname = 'Lesson';
+$tablename = 'news';
+$db = new DataB('localhost', 'root', '', 'Lesson');
