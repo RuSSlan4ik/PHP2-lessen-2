6c63aa8c8693ce394219d6b6245a436a20afacae
RuSSlan4ik <ru.hus@mail.ru>
2015-04-13 13:03:03 +0300

    база

--- models/db.php
+++ models/db.php
@@ -6,6 +6,6 @@ require_once __DIR__ . '/../classes/News.inc';
 $anews = new News();
 $tablename = $anews->setTableName();
 $sql = $anews->getAllData($tablename);
-$namedb = 'NewsDb';
+$namedb = 'Lesson';
 $db = new DataB(DataB::DATABASE_HOST, DataB::DATABASE_USER, DataB::DATABASE_PSW, $namedb);
 $news = $db->getData($sql);
