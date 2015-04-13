cadef1a283b8ff5ac5871186122567c3c80d2629
RuSSlan4ik <ru.hus@mail.ru>
2015-04-13 12:12:35 +0300

    создание объектов

--- models/news.php
+++ models/news.php
@@ -1,16 +1,13 @@
 <?php
 require __DIR__ . '/db.php';
 
-function findAllNews()
-{
-  $sql = 'SELECT * FROM news ORDER BY date DESC';
-  $ret = dbFindAllByQuery($sql);
-  return $ret;
-}
+require_once __DIR__ . '/../classes/Data.inc';
+require_once __DIR__ . '/../classes/Article.inc';
+require_once __DIR__ . '/../classes/News.inc';
 
-function findOneById($id)
-{
-  $sql = 'SELECT * FROM news WHERE id=' . $id;
-  $ret = dbFindOneByQuery($sql);
-  return $ret;
-}
+$anews = new News($title, $text, $author, $date);
+$tablename = $anews->setTableName();
+$sql = $anews->addRecord($tablename);
+$namedb = 'NewsDb';
+$db = new DataB(DataB::DATABASE_HOST, DataB::DATABASE_USER, DataB::DATABASE_PSW, $namedb);
+$news = $db->changeData($sql);
