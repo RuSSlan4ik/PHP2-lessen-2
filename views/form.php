998bb84174253032a20a4c75f5df0d61b0f6e39d
RuSSlan4ik <ru.hus@mail.ru>
2015-04-13 10:48:22 +0300

    переименование имен

--- views/form.php
+++ views/form.php
@@ -8,11 +8,11 @@
 <h1>Форма добавления новости</h1>
 
 <form action="../form.php" method="post">
-  <p><input type="text" name="author_name" placeholder="Ваше имя" size="25"/></p>
+  <p><input type="text" name="author" placeholder="Ваше имя" size="25"/></p>
 
   <p><input type="text" name="title" placeholder="Заголовок новости" size="25"/></p>
 
-  <p><textarea name="text_news" cols="100" rows="8" placeholder="Текст новости"></textarea></p>
+  <p><textarea name="text" cols="100" rows="8" placeholder="Текст новости"></textarea></p>
 
   <p><button role="button" type="submit">Отправить</button></p>
 </form>
