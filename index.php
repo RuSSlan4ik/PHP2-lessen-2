b34085f13f90bfbad1290c85bce44c7175cddb1d
RuSSlan4ik <ru.hus@mail.ru>
2015-04-16 00:30:01 +0300

    переделано2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require __DIR__ . '/models/NewsArticle.php';
 $news = new NewsArticle();
-var_dump($news->findOne($id=2));
+//var_dump($news->findAll());
 include __DIR__ . '/views/index.php';
