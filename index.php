fe8a35ccda93c45c1b0740cd37399fd9f3e7e338
RuSSlan4ik <ru.hus@mail.ru>
2015-04-16 19:58:09 +0300

    бред

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
-require __DIR__ . '/models/NewsArticle.php';
-$news = new NewsArticle();
-//var_dump($news->findAll());
-include __DIR__ . '/views/index.php';
+
+$ctrl = !empty($_GET['ctrl']) ? $_GET['ctrl'] : 'news';
+$ctrlClassName = ucfirst($ctrl) . 'Controller';
+
+require __DIR__ . '/controllers/' . $ctrlClassName . '.php';
+
+$action = !empty($_GET['action']) ? $_GET['action'] : 'all';
+$actionName = 'action' . ucfirst($action);
+
+If (!empty ($_GET['id']) && ($_GET['action'] == 'one')) {
+
+  $news = new $ctrlClassName();
+  $news->$actionName();
+}
+elseIf ($_GET['action'] == 'form') {
+    $admin = new $ctrlClassName();
+    $admin->$actionName();
+
+  }
+else{
+  $controllers = new $ctrlClassName;
+  $controllers->$actionName();
+}
