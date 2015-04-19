5fdfad9c5a3ad0c29e19d98b50d99a66163abf30
RuSSlan4ik <ru.hus@mail.ru>
2015-04-19 21:10:29 +0300

    создание

--- index.php
+++ index.php
@@ -8,17 +8,5 @@ require __DIR__ . '/controllers/' . $ctrlClassName . '.php';
 $action = !empty($_GET['action']) ? $_GET['action'] : 'all';
 $actionName = 'action' . ucfirst($action);
 
-If (!empty ($_GET['id']) && ($_GET['action'] == 'one')) {
-
-  $news = new $ctrlClassName();
-  $news->$actionName();
-}
-elseIf ($_GET['action'] == 'form') {
-    $admin = new $ctrlClassName();
-    $admin->$actionName();
-
-  }
-else{
   $controllers = new $ctrlClassName;
   $controllers->$actionName();
-}
