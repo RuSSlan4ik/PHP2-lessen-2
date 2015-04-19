f0865f3bba40ef3dce2d26b0a096db497ce94bd5
RuSSlan4ik <ru.hus@mail.ru>
2015-04-19 21:17:54 +0300

    создание

---
+++ classes/View.php
@@ -0,0 +1,15 @@
+<?php
+
+class View
+{
+  public $items;
+  public function getTemplatePath()
+  {
+    return __DIR__.'/../view/';
+  }
+  public  function render($template, $data)
+  {
+    extract($data);
+    require $this->getTemplatePath().'/'.$template.'.php';
+  }
+}
