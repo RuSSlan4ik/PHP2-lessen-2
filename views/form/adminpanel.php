5a2503cbaf237b3ae77ed07007e7a3204b66ddbd
RuSSlan4ik <ru.hus@mail.ru>
2015-04-26 21:40:47 +0300

    добавлен admin panel

---
+++ views/form/adminpanel.php
@@ -0,0 +1,22 @@
+
+/*
+[21:22:26] Phil: как-то так
+[21:22:34] Phil: во view, разумеется
+*/
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>admin panel</title>
+</head>
+<body>
+<h1>
+
+
+  </*?php if ($app->user->isAdmin()) : */?>
+
+
+</h1>
+<div><a href="/index.php">Назад</a></div>
+</body>
+</html>
